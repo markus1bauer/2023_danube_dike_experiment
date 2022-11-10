dbb12ff7a7762afe845216998a43aed5ab0ea76e
markus1bauer <markus1.bauer@tum.de>
2022-11-10 11:11:58 +0100

    Update FCS model

--- R/model__fcs_target.R
+++ R/model__fcs_target.R
@@ -169,9 +169,9 @@ chains = 4
 thin = 2
 priors <- c(
   set_prior("normal(0, 1)", class = "b"),
-  set_prior("normal(0.1, 1)", class = "b", coef = "expositionnorth"),
   set_prior("normal(0.1, 1)", class = "b", coef = "sand_ratio25"),
   set_prior("normal(0.2, 1)", class = "b", coef = "sand_ratio50"),
+  set_prior("normal(0.1, 1)", class = "b", coef = "expositionnorth"),
   set_prior("normal(0.1, 1)", class = "b", coef = "survey_year_fct2019"),
   set_prior("normal(0.2, 1)", class = "b", coef = "survey_year_fct2020"),
   set_prior("normal(0.3, 1)", class = "b",coef = "survey_year_fct2021"),
@@ -184,13 +184,13 @@ get_prior(n ~ target_type + exposition + sand_ratio + survey_year_fct +
             (1 | site/plot) + (1 | botanist_year),
           data = sites)
 ### Example of normal distribution ###
-ggplot(data = data.frame(x = c(-3, 3)), aes(x = x)) +
+ggplot(data = data.frame(x = c(-2, 2)), aes(x = x)) +
   stat_function(fun = dnorm, n = 101, args = list(mean = 0.1, sd = 1))
 ### Example of cauchy distribution ###
-ggplot(data = data.frame(x = c(-3, 3)), aes(x = x)) +
+ggplot(data = data.frame(x = c(-2, 2)), aes(x = x)) +
   stat_function(fun = dcauchy, n = 101, args = list(location = 0, scale = 1))
 ### Example of a student t distribution ###
-ggplot(data.frame(x = c(-3, 3)), aes(x = x)) +
+ggplot(data.frame(x = c(-2, 2)), aes(x = x)) +
   stat_function(fun = dstudent_t, args = list(df = 3, mu = 0, sigma = 2.5))
 
 
@@ -238,6 +238,25 @@ m1 <- brm(n ~ (target_type + exposition + sand_ratio + survey_year_fct)^4 +
            cores = parallel::detectCores(),
            seed = 123)
 
+m3 <- brm(n ~ (target_type + exposition + sand_ratio + survey_year_fct)^2 +
+                 substrate_depth + seed_density +
+                 substrate_depth:sand_ratio +
+                 seed_density:exposition +
+                 target_type:exposition:survey_year_fct +
+                 sand_ratio:exposition:survey_year_fct +
+                 seed_density:exposition:survey_year_fct +
+                 (1 | site/plot) + (1 | botanist_year),
+               data = sites, 
+               family = gaussian("identity"),
+               prior = priors,
+               chains = chains,
+               iter = iter,
+               thin = thin,
+               warmup = floor(iter / 2),
+               save_pars = save_pars(all = TRUE),
+               cores = parallel::detectCores(),
+               seed = 123)
+
 m_full_flat <- brm(n ~ (target_type + exposition + sand_ratio + survey_year_fct +
                       seed_density + substrate_depth)^3 +
                  target_type:sand_ratio:exposition:survey_year_fct +
@@ -277,30 +296,10 @@ m3_flat <- brm(n ~ (target_type + exposition + sand_ratio + survey_year_fct)^2 +
                 cores = parallel::detectCores(),
                 seed = 123)
 
-m3_iter <- brm(n ~ (target_type + exposition + sand_ratio + survey_year_fct)^2 +
-            substrate_depth + seed_density +
-            substrate_depth:sand_ratio +
-            seed_density:exposition +
-            target_type:exposition:survey_year_fct +
-            sand_ratio:exposition:survey_year_fct +
-            seed_density:exposition:survey_year_fct +
-            (1 | site/plot) + (1 | botanist_year),
-          data = sites, 
-          family = gaussian("identity"),
-          prior = priors,
-          chains = chains,
-          iter = 10000,
-          thin = thin,
-          warmup = floor(iter / 2),
-          save_pars = save_pars(all = TRUE),
-          cores = parallel::detectCores(),
-          seed = 123)
 
 ### b comparison ------------------------------------------------------------
 
-m_1 <- m_simple
-#m_1 <- m3_flat
-m_1 <- m3_iter
+m_1 <- m1
 m_2 <- m3
 bayes_R2(m_1, probs = c(0.05, 0.5, 0.95),
          re_formula =  ~ (1 | site/plot) + (1 | botanist_year)) 
@@ -335,12 +334,15 @@ posterior1 <- m_1 %>%
   posterior::as_draws() %>%
   posterior::subset_draws(
     variable = c(
-      "b_substrate_depth30",
-      "b_seed_density8",
-      "b_target_typedry_grassland",
       "b_sand_ratio25",
       "b_sand_ratio50",
+      "b_substrate_depth30",
+      "b_target_typedry_grassland",
+      "b_seed_density8",
       "b_expositionnorth",
+      "b_survey_year_fct2019",
+      "b_survey_year_fct2020",
+      "b_survey_year_fct2021",
       "sd_site__Intercept",
       "sd_site:plot__Intercept",
       "sd_botanist_year__Intercept",
@@ -351,12 +353,15 @@ posterior2 <- m_2 %>%
   posterior::as_draws() %>%
   posterior::subset_draws(
     variable = c(
-      "b_substrate_depth30",
-      "b_seed_density8",
-      "b_target_typedry_grassland",
       "b_sand_ratio25",
       "b_sand_ratio50",
+      "b_substrate_depth30",
+      "b_target_typedry_grassland",
+      "b_seed_density8",
       "b_expositionnorth",
+      "b_survey_year_fct2019",
+      "b_survey_year_fct2020",
+      "b_survey_year_fct2021",
       "sd_site__Intercept",
       "sd_site:plot__Intercept",
       "sd_botanist_year__Intercept",
@@ -370,8 +375,6 @@ yrep1 <- posterior_predict(m_1, draws = 500)
 yrep2 <- posterior_predict(m_2, draws = 500)
 loo1 <- loo(m_1, save_psis = TRUE, moment_match = FALSE)
 loo2 <- loo(m_2, save_psis = TRUE, moment_match = FALSE)
-
-#### * Samling efficency/effectiveness (Rhat and EFF) ####
 draws1 <- m_1 %>%
   posterior::as_draws() %>%
   posterior::summarize_draws() %>%
@@ -380,6 +383,8 @@ draws2 <- m_2 %>%
   posterior::as_draws() %>%
   posterior::summarize_draws() %>%
   filter(str_starts(variable, "b_"))
+
+#### * Samling efficency/effectiveness (Rhat and EFF) ####
 range(draws1$rhat)
 range(draws2$rhat)
 range(draws1$ess_bulk)
@@ -455,7 +460,7 @@ mcmc_acf(posterior2, lags = 10)
 
 ### a Model output ------------------------------------------------------------
 
-prior_summary(m_simple)
+prior_summary(m_1)
 bayes_R2(m_1, probs = c(0.05, 0.5, 0.95),
          re_formula =  ~ (1 | site/plot) + (1 | botanist_year)) 
 bayes_R2(m_2, probs = c(0.05, 0.5, 0.95),
@@ -466,22 +471,20 @@ bayes_R2(m_2, probs = c(0.05, 0.5, 0.95),
          re_formula = 1 ~ 1)
 draws1
 mcmc_intervals(
-  posterior2,
+  posterior1,
   prob = 0.5,
   prob_outer = 0.95,
   point_est = "mean"
 )
-sjPlot::plot_model(m3, type = "est")
-sjPlot::plot_model(m3_flat, type = "est")
-sjPlot::plot_model(m_1, type = "pred", ppd = TRUE, terms = c(
-  "sand_ratio", "substrate_depth", "survey_year_fct", "exposition"
-))
-sjPlot::plot_model(m_1, type = "pred", ppd = TRUE, terms = c(
-  "sand_ratio", "target_type", "survey_year_fct", "exposition"
-  ))
-sjPlot::plot_model(m_1, type = "pred", ppd = TRUE, terms = c(
-  "exposition", "seed_density", "survey_year_fct"
-))
+sjPlot::plot_model(m_1, m_2, type = "est")
+sjPlot::plot_model(m_2, type = "est")
+sjPlot::plot_model(
+  m_1, m_2, type = "est", ppd = TRUE,
+  terms = c(
+    "sand_ratio", "substrate_depth", "target_type",
+    "seed_density",  "exposition", "survey_year_fct"
+    )
+  )
 
 
 ### b Effect sizes ------------------------------------------------------------
