75a69812871de40ab030813bc30f985c6607d0b4
Markus Bauer <markus1.bauer@tum.de>
2023-02-09 14:59:04 +0100

    Update _prepare_data_spatial.R

--- R/_prepare_data_spatial.R
+++ R/_prepare_data_spatial.R
@@ -1,11 +1,15 @@
+# Grassland experiment on dikes
 # Prepare spatial data ####
+
 # Markus Bauer
+# 2023-02-09
 
 
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# A Preparation ################################################################################################################
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# A Preparation ################################################################
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 
 ### Packages ###
@@ -19,19 +23,23 @@ library(mapedit)
 
 ### Start ###
 rm(list = ls())
-setwd(here("data/raw/spatial"))
+setwd(here("data", "raw", "spatial"))
 register_google(key = "AIzaSyB5nQU_dgB_kPsQkk-_cq7pA0g1-2qka4E")
 
 
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# B Load shp files ##########################################################################################################
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# B Load shp files #############################################################
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-## 1 Base maps #################################################################################################
 
-germany <- raster::getData('GADM', country = 'DEU', level = 0, download =  T) %>% 
+## 1 Base maps #################################################################
+
+
+germany <- raster::getData(
+  'GADM', country = 'DEU', level = 0, download =  TRUE
+) %>% 
   st_as_sf() %>%
   st_set_crs(4326)
 
@@ -64,8 +72,10 @@ raster <- brick(raster(gmapr), raster(gmapg), raster(gmapb))
 rm(gmapr, gmapg, gmapb)
 rgmaprgbGM <- raster
 projection(rgmaprgbGM) <- CRS("+init=epsg:3857")
-rprobextSpDF <- as(extent(unlist(attr(bg_google_satellite, which = "bb"))[c(2, 4, 1, 3)]), 
-                   "SpatialPolygons")
+rprobextSpDF <- as(
+  extent(unlist(attr(bg_google_satellite, which = "bb"))[c(2, 4, 1, 3)]),
+  "SpatialPolygons"
+  )
 projection(rprobextSpDF) <- CRS("+init=epsg:4326")
 rprobextGM <- spTransform(rprobextSpDF, CRS("+init=epsg:3857"))
 extent(rgmaprgbGM) <- c(rprobextGM@bbox[1, ], rprobextGM@bbox[2, ])
@@ -73,7 +83,9 @@ bg_google_satellite2 <- rgmaprgbGM
 rm(mgmap, raster, rgmaprgbGM, rprobextGM, rprobextSpDF, vgmaprgb, vgmap)
 
 
-## 2 Transform shp files #################################################################################################
+
+## 2 Transform shp files #######################################################
+
 
 dikes <- st_read("Deich.shp") %>%
   st_transform(crs = 4326) %>%
@@ -89,15 +101,25 @@ blocks <- st_read("blocks.kml") %>%
   st_convex_hull %>%
   rename(block = Name) %>%
   mutate(block = as_factor(block)) %>%
-  mutate(block = fct_recode(block, "6" = "1", "5" = "2", "4" = "3", "3" = "4", "2" = "5", "1" = "6"))
+  mutate(block = fct_recode(
+    block,
+    "6" = "1", "5" = "2", "4" = "3", "3" = "4", "2" = "5", "1" = "6"
+    ))
 
-blocks2 <- blocks %>%
+blocks_table <- blocks %>%
   st_centroid()
-blocks2 <- bind_cols(as_tibble(as_factor(st_drop_geometry(blocks2))), as_tibble(st_coordinates(blocks2))) %>%
+blocks_table <- bind_cols(
+  as_tibble(as_factor(st_drop_geometry(blocks_table))),
+  as_tibble(st_coordinates(blocks_table))
+  ) %>%
   rename(x = X, y = Y)
 
-## 3 Digitize shp files #################################################################################################
 
+
+## 3 Digitize shp files ########################################################
+
+
+### Digitized only once: ###
 #data <- mapview() %>% editMap()
 #mapview(data$finished)
 #danube <- data$finished %>%
@@ -107,25 +129,62 @@ blocks2 <- bind_cols(as_tibble(as_factor(st_drop_geometry(blocks2))), as_tibble(
   #mutate(river = str_extract(river, "Danube")) %>%
   #st_crop(ymin = 48.835, ymax = 48.845, xmin = 12.87, xmax = 12.895)
 #plot(st_geometry(danube))
-rm(data)
 
 
   
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# C Save ##############################################################################
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# C Save ######################################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-setwd(here("data/processed/spatial"))
-write_csv2(blocks2, file = "blocks2.csv")
-writeRaster(bg_google_satellite2, file = "bg_google_satellite2.grd", overwrite = TRUE, 
-            datatype = "raster")
-st_write(germany, layer = "germany.shp", driver = "ESRI Shapefile", delete_layer = T,
-         dsn = here("data/processed/spatial"))
-#st_write(danube, layer = "danube.shp", driver = "ESRI Shapefile", delete_layer = T,
-         #dsn = here("data/processed/spatial"))
-st_write(dikes, layer = "dikes.shp", driver = "ESRI Shapefile", delete_layer = T,
-         dsn = here("data/processed/spatial"))
-st_write(blocks, layer = "blocks.shp", driver = "ESRI Shapefile", delete_layer = T,
-         dsn = here("data/processed/spatial"))
-save(bg_google_satellite, file = paste0(here("data/processed/spatial"), "/", "bg_google_satellite.rda"))
-save(bg_stamen_terrain, file = paste0(here("data/processed/spatial"), "/", "bg_stamen_terrain.rda"))
+
+
+write_csv2(blocks_table, file = "blocks2.csv")
+writeRaster(
+  bg_google_satellite2,
+  file = paste0(
+    here("data", "processed", "spatial"), "/", "bg_google_satellite2.grd"
+  ),
+  overwrite = TRUE, 
+  datatype = "raster"
+)
+st_write(
+  germany,
+  layer = "germany.shp",
+  driver = "ESRI Shapefile",
+  delete_layer = TRUE,
+  dsn = here("data", "processed", "spatial")
+)
+### Digitized only once: ###
+#st_write(
+#  danube,
+#  layer = "danube.shp",
+#  driver = "ESRI Shapefile",
+#  delete_layer = TRUE,
+#  dsn = here("data", "processed", "spatial")
+#)
+st_write(
+  dikes,
+  layer = "dikes.shp",
+  driver = "ESRI Shapefile",
+  delete_layer = TRUE,
+  dsn = here("data", "processed", "spatial")
+)
+st_write(
+  blocks,
+  layer = "blocks.shp",
+  driver = "ESRI Shapefile",
+  delete_layer = TRUE,
+  dsn = here("data", "processed", "spatial")
+)
+save(
+  bg_google_satellite,
+  file = paste0(
+    here("data", "processed", "spatial"), "/", "bg_google_satellite.rda"
+    )
+)
+save(
+  bg_stamen_terrain,
+  file = paste0(
+    here("data", "processed", "spatial"), "/", "bg_stamen_terrain.rda"
+    )
+)
