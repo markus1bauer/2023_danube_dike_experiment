e9e833deabefaaeb553ccb45153ffc656a787586
Markus Bauer <markus1.bauer@tum.de>
2023-02-28 10:45:52 +0100

    Update _prepare_data_spatial.R

--- R/_prepare_data_spatial.R
+++ R/_prepare_data_spatial.R
@@ -2,7 +2,7 @@
 # Prepare spatial data ####
 
 # Markus Bauer
-# 2023-02-09
+# 2023-02-28
 
 
 
@@ -23,8 +23,6 @@ library(mapedit)
 
 ### Start ###
 rm(list = ls())
-setwd(here("data", "raw", "spatial"))
-register_google(key = "AIzaSyB5nQU_dgB_kPsQkk-_cq7pA0g1-2qka4E")
 
 
 
@@ -43,68 +41,36 @@ germany <- raster::getData(
   st_as_sf() %>%
   st_set_crs(4326)
 
-bg_google_satellite <- get_map(
-  location = c(lon = 12.886, lat = 48.839), 
-  zoom = 15,
-  scale = 1,
-  maptype = "satellite",
-  color = "color",
-  source = "google"
-  )
-ggmap(bg_google_satellite)
-
 bg_stamen_terrain <- get_map(
+  source = "stamen",
+  maptype = "terrain",
   location= c(left = 12.87, bottom = 48.835, right = 12.895, top = 48.845),
-  zoom = 14, 
-  scale = 1,
-  maptype = "terrain-background",
-  source = "stamen"
+  zoom = 14,
+  scale = 1
 )
 ggmap(bg_stamen_terrain)
 
-mgmap <- as.matrix(bg_google_satellite)
-vgmap <- as.vector(mgmap)
-vgmaprgb <- col2rgb(vgmap)
-gmapr <- matrix(vgmaprgb[1, ], ncol = ncol(mgmap), nrow = nrow(mgmap))
-gmapg <- matrix(vgmaprgb[2, ], ncol = ncol(mgmap), nrow = nrow(mgmap))
-gmapb <- matrix(vgmaprgb[3, ], ncol = ncol(mgmap), nrow = nrow(mgmap))
-raster <- brick(raster(gmapr), raster(gmapg), raster(gmapb))
-rm(gmapr, gmapg, gmapb)
-rgmaprgbGM <- raster
-projection(rgmaprgbGM) <- CRS("+init=epsg:3857")
-rprobextSpDF <- as(
-  extent(unlist(attr(bg_google_satellite, which = "bb"))[c(2, 4, 1, 3)]),
-  "SpatialPolygons"
-  )
-projection(rprobextSpDF) <- CRS("+init=epsg:4326")
-rprobextGM <- spTransform(rprobextSpDF, CRS("+init=epsg:3857"))
-extent(rgmaprgbGM) <- c(rprobextGM@bbox[1, ], rprobextGM@bbox[2, ])
-bg_google_satellite2 <- rgmaprgbGM
-rm(mgmap, raster, rgmaprgbGM, rprobextGM, rprobextSpDF, vgmaprgb, vgmap)
 
 
 
-## 2 Transform shp files #######################################################
+## 2 Load and transform shp files ##############################################
 
 
-dikes <- st_read("Deich.shp") %>%
+dikes <- st_read(here("data", "raw", "spatial", "Deich.shp")) %>%
   st_transform(crs = 4326) %>%
   st_crop(ymin = 48.835, ymax = 48.845, xmin = 12.87, xmax = 12.895)
-### change existing shp-file ###
-#data <- mapview(st_geometry(blocks)) %>%
-  #editMap(targetLayerId = "blocks")
-#mapview(data$drawn)
 
-blocks <- st_read("blocks.kml") %>%
+blocks <- st_read(here("data", "raw", "spatial", "blocks.kml")) %>%
   group_by(Name) %>%
   summarise(geometry = st_combine(geometry)) %>%
-  st_convex_hull %>%
+  st_convex_hull() %>%
   rename(block = Name) %>%
-  mutate(block = as_factor(block)) %>%
-  mutate(block = fct_recode(
-    block,
-    "6" = "1", "5" = "2", "4" = "3", "3" = "4", "2" = "5", "1" = "6"
-    ))
+  mutate(
+    block = as_factor(block),
+    block = fct_recode(
+      block, "6" = "1", "5" = "2", "4" = "3", "3" = "4", "2" = "5", "1" = "6"
+      )
+    )
 
 blocks_table <- blocks %>%
   st_centroid()
@@ -122,12 +88,7 @@ blocks_table <- bind_cols(
 ### Digitized only once: ###
 #data <- mapview() %>% editMap()
 #mapview(data$finished)
-#danube <- data$finished %>%
-  #st_as_sf() %>%
-  #rename(river = X_leaflet_id) %>%
-  #mutate(river = str_replace(as.character(river), ".", "Danube")) %>%
-  #mutate(river = str_extract(river, "Danube")) %>%
-  #st_crop(ymin = 48.835, ymax = 48.845, xmin = 12.87, xmax = 12.895)
+#danube <- data$finished %>% st_as_sf()
 #plot(st_geometry(danube))
 
 
@@ -138,17 +99,9 @@ blocks_table <- bind_cols(
 
 
 
-write_csv2(
+write_csv(
   blocks_table,
   file = here("data", "processed", "spatial", "blocks_table.csv")
-)
-writeRaster(
-  bg_google_satellite2,
-  file = paste0(
-    here("data", "processed", "spatial"), "/", "bg_google_satellite2.grd"
-  ),
-  overwrite = TRUE, 
-  datatype = "raster"
 )
 st_write(
   germany,
@@ -157,7 +110,7 @@ st_write(
   delete_layer = TRUE,
   dsn = here("data", "processed", "spatial")
 )
-### Digitized only once: ###
+### Digitized shp saved only once: ###
 #st_write(
 #  danube,
 #  layer = "danube.shp",
@@ -178,12 +131,6 @@ st_write(
   driver = "ESRI Shapefile",
   delete_layer = TRUE,
   dsn = here("data", "processed", "spatial")
-)
-save(
-  bg_google_satellite,
-  file = paste0(
-    here("data", "processed", "spatial"), "/", "bg_google_satellite.rda"
-    )
 )
 save(
   bg_stamen_terrain,
